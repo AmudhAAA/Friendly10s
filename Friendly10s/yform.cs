17676a0ae0d991fa86d3e9d5dea7270258fe3c81
Yiqing Zhang <yiqing.zhang3@education.nsw.gov.au>
2022-07-26 14:42:12 +1000

    jjiojooi

---
+++ Friendly10s/yform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Friendly10s
+{
+    public partial class yform : Form
+    {
+        public yform()
+        {
+            InitializeComponent();
+        }
+    }
+}
